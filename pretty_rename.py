baddfb5804105d8d5ae0c0fe4e5ea900123dc9a1
Jean-Ray Arseneau <git@jeanray.arseneau.ca>
2015-03-04 00:21:27 -0500

    Fixed timezone bug in article date with Pelican 3.5

--- pretty_rename.py
+++ pretty_rename.py
@@ -4,7 +4,7 @@ Auto Rename
 
 This plugin will auto rename content files to the format:
 	YYYY-MM-DD-<slug>.ext
-	
+
 ========================
 
 """
@@ -18,11 +18,12 @@ def rename_content(generator):
 	for article in generator.articles:
 		(a_filepath, a_filename) = path.split(article.source_path)
 		a_extension = path.splitext(article.source_path)[1]
-		a_date_struct = strptime(str(article.date), "%Y-%m-%d %H:%M:%S")
+		print(str(article.date))
+		a_date_struct = strptime(str(article.date)[:-6], "%Y-%m-%d %H:%M:%S")
 		a_new_filename = a_filepath + "/" + strftime("%Y-%m-%d-", a_date_struct) + article.slug + a_extension
 
 		if not (article.source_path == a_new_filename):
 			rename(article.source_path, a_new_filename)
-				
+
 def register():
 	signals.article_generator_finalized.connect(rename_content)
